910172eebc0955e95fb58edb6d4c0eea5f3115b8
Aaron Klotz <aklotz@mozilla.com>
2015-08-11 14:16:31 -0600

    some cleanup

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cl -Zi -EHsc -DNTDDI_VERSION=0x06030000 -D_WIN32_WINNT=0x0603 glass.cpp user32.lib gdi32.lib dwmapi.lib uxtheme.lib shcore.lib
+cl -Zi -EHsc -DNTDDI_VERSION=0x06030000 -D_WIN32_WINNT=0x0603 -DUNICODE -D_UNICODE glass.cpp glasswnd.cpp user32.lib gdi32.lib dwmapi.lib uxtheme.lib shcore.lib
